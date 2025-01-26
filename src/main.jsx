4318eee11a79716876de386f4cf5eb1bdd441526
Julianna Fernandes <juliannabf29@gmail.com>
2025-01-26 14:12:22 -0300

    feat: criando NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import AppRoutes from '../src/routes/routes';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+    <AppRoutes/>
+  </StrictMode>
+);
---
+++ src/routes/routes.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrimarySearchAppBar from '../components/NavBar';
+import Dashboard from "../pages/Dashboard";
+import Products from "../pages/Product";
+import Items from "../pages/Item";
+import Carts from "../pages/Cart";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+        <PrimarySearchAppBar />
+      <Routes>
+        
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/carts" element={<Carts />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
